e8058808639e83e603f0e8ee5f09ecaa5af64187
HRITIK SAXENA <saxenahritik22@gmail.com>
2026-03-24 22:03:39 +0530

    CORS changes

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,7 +21,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[settings.FRONTEND_URL, "http://localhost:5173", "http://localhost:3000"],
+    allow_origins=[settings.FRONTEND_URL, "http://localhost:5173", "http://localhost:3000","https://ai-edition-hrms-portal.onrender.com/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
